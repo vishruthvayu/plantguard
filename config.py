565e7094b823432c3230c2c8ff8f86859c594473
student vishruthvayu <vishruthvayu@gmail.com>
2026-05-02 18:14:26 +0530

    feat: add transforms pipeline and DataLoader with train/val split

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import torch 
-
+from torchvision import transforms
 DEVICE = "cuda" if torch.is_vulkan_available() else "cpu"
 
 DATA_DIR = "data/plantvillage dataset/color"
@@ -10,3 +10,18 @@ NUM_WORKERS = 2
 LEARNING_RATE = 1e-3
 
 NUM_CLASSES = 38
+
+TRAIN_TRANSFORMS = transforms.Compose([
+    transforms.Resize((IMAGE_SIZE,IMAGE_SIZE)),
+    transforms.RandomHorizontalFlip(),
+    transforms.RandomRotation(15),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485,0.456,0.406],std=[0.229,0.224,0.225])
+    ])
+
+TEST_TRANSFORMS = transforms.Compose([
+    transforms.Resize((IMAGE_SIZE,IMAGE_SIZE)),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485,0.456,0.406],std=[0.229,0.224,0.225])
+    ])
+
--- main.py
+++ main.py
@@ -1,10 +1,25 @@
 from src.dataset import PlantDiseaseDataset
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader,random_split
 import config
 
-dataset = PlantDiseaseDataset(root_dir=config.DATA_DIR, transform=None)
+dataset = PlantDiseaseDataset(root_dir=config.DATA_DIR, transform=config.TRAIN_TRANSFORMS)
 
-print(f"Total images: {len(dataset)}")
-print(f"Total classes: {len(dataset.classes)}")
-print(f"first 3 classes: {dataset.classes[:3]}")
-print(f"Sample entry: {dataset.samples[0]}")
+train_size = int(0.8 * len(dataset))
+val_size = len(dataset) - train_size
+
+train_dataset, val_dataset = random_split(dataset,[train_size,val_size])
+
+train_loader = DataLoader(train_dataset,batch_size=config.BATCH_SIZE,shuffle=True)
+val_loader = DataLoader(val_dataset,batch_size=config.BATCH_SIZE,shuffle=False)
+
+print(f"Train samples: {len(train_dataset)}")
+print(f"validation samples: {len(val_dataset)}")
+
+images,labels = next(iter(train_loader))
+print(f"Batch image shape: {images.shape}")
+print(f"Batch label shape: {labels.shape}")
+
+# print(f"Total images: {len(dataset)}")
+# print(f"Total classes: {len(dataset.classes)}")
+# print(f"first 3 classes: {dataset.classes[:3]}")
+# print(f"Sample entry: {dataset.samples[0]}")
